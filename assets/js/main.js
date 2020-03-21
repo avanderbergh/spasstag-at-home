7e66e067ed4a2255caff16c2ecb08858b4669d75
avanderbergh <avanderbergh@gmail.com>
2020-03-21 15:50:38 +0100

    Added timer
    Co-authored-by: Felix Seeger <felixseeger@googlemail.com>
    Co-authored-by: Alkhwaldh <khwaldh89@gmail.com>
    Co-authored-by: LayorDaymor <Ahmad.karbouj98@gmail.com>
    Co-authored-by: Natalie Streich <natalie@super-code.de>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,30 @@
 const DOC_KEY = "1Qbu3hsAlxNS6ybsjzhLVGFtv287diwiD2tL8ZkMXvOE";
+const TYPES = ["Kreativ", "Bewegung", "Denken / Träumen"];
+
+function startGame() {
+	document.getElementById("instructions").style.display = "none";
+	document.getElementById("timer").style.display = "block";
+}
+
+function skipGame() {
+	location.reload();
+}
+
+function startTimer(length) {
+	length = length - 1;
+	document.getElementById("minutes").innerText = length;
+	if (length > 0) {
+		setTimeout(() => startTimer(length), 60000);
+	}
+}
 
 window.addEventListener("load", e => {
-	GetSheetDone.raw(DOC_KEY).then(sheet => {
-		console.log(sheet);
+	GetSheetDone.labeledCols(DOC_KEY, 1).then(sheet => {
+		const games = sheet.data;
+		const game = games[Math.floor(Math.random() * games.length)];
+
+		document.getElementById("icon").src = game.icon;
+		document.getElementById("explanation").innerText = game.explanation;
+		document.getElementById("type").innerText = TYPES[game.type - 1];
 	});
 });
